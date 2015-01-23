c85ec88c4215d1b7ffa58862833cd3453f1ffab4
Derek Stride <djgstride@gmail.com>
2015-01-23 18:34:46 -0500

    Basic File added that will provide the general schematic on how to code the microcontroller to send messages to the screed

---
+++ simple.cpp
@@ -0,0 +1,60 @@
+#include <LiquidCrystal.h>
+#include <Wire.h>
+
+// define the data bus pins
+#define d0 9     // connected to D4
+#define d1 10    // connected to D5
+#define d2 11    // connected to D6
+#define d3 12    // connected to D7
+
+// define the en (enable) and rs (read signal) pins
+#define rs 7     // tells the lcd if a character or command is on the data bus
+#define en 8    // tells the lcd if the data is stable on the data bus
+
+
+// instantiate an lcd object with the pins we used.
+LiquidCrystal lcd(rs, en, d0, d1, d2, d3);
+
+
+void setup()
+{
+    // set up the LCD with columns, rows
+
+    lcd.begin(16, 2);
+
+
+    // Setup pins as input
+    pinMode(2, IN);
+    pinMode(4, IN);
+
+    lcd.print("  Message Top!  ");
+    lcd.setCursor(0, 1);
+    lcd.print(" Message Bottom ");
+
+}
+
+
+void loop()
+{
+
+    if ( digitalRead( inputs.at(0) ) )
+    {
+        lcd.print("  Message Top2  ");
+        lcd.setCursor(0, 1);
+        lcd.print(" Message Bottom ");
+    }
+    else if ( digitalRead( inputs.at(1) ) )
+    {
+        lcd.print("  Message Top3  ");
+        lcd.setCursor(0, 1);
+        lcd.print(" Message Bottom ");
+    }
+    else if ( digitalRead( inputs.at(0) ) & digitalRead( inputs.at(1) ) )
+    {
+        lcd.print("  Message Top4  ");
+        lcd.setCursor(0, 1);
+        lcd.print(" Message Bottom ");
+    }
+
+
+}
