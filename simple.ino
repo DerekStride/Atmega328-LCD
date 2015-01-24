845a335c88d7fec5d36e90678371078299837722
Derek Stride <djgstride@gmail.com>
2015-01-24 05:32:16 +0000

    This is the code runing on the chip successfully.

--- simple.ino
+++ simple.ino
@@ -1,41 +1,14 @@
 #include <LiquidCrystal.h>
 #include <Wire.h>
 
-// define the data bus pins
-#define d0 9     // connected to D4
-#define d1 10    // connected to D5
-#define d2 11    // connected to D6
-#define d3 12    // connected to D7
+LiquidCrystal lcd(7, 8, 9, 10, 4, 2);
 
-// define the en (enable) and rs (read signal) pins
-#define rs 7     // tells the lcd if a character or command is on the data bus
-#define en 8    // tells the lcd if the data is stable on the data bus
-
-
-// instantiate an lcd object with the pins we used.
-LiquidCrystal lcd(rs, en, d0, d1, d2, d3);
-
-
-void setup()
-{
-    // set up the LCD with columns, rows
-
-    lcd.begin(16, 2);
-
-
-    // Setup pins as input
-    pinMode(2, IN);
-    pinMode(4, IN);
-
-    // Provide messages to the screen see function below for the implementation
-    lcd.printMessage("  Message Top!  ", " Message Bottom ");
-
-}
+int brightness = 255;
 
 /*
  *  Change this function to have messages for each line on the display and keep incrementing the row in the setCursor method each time you move to the next line.
  */
-void printMessage(string mess1, string mess2)
+void printMessage(String mess1, String mess2)
 {
     lcd.clear();
     lcd.print(mess1);
@@ -43,19 +16,22 @@ void printMessage(string mess1, string mess2)
     lcd.print(mess2);
 }
 
+void setup()
+{
+	Serial.begin(9600);
+	lcd.begin(16, 2);
+
+	pinMode(1, INPUT_PULLUP);
+	
+	printMessage("RGB 16x2 Display", "Sweetness!");
+}
 
 void loop()
 {
 
-    if ( digitalRead( inputs.at(0) ) )
-    {
-        printMessage("  Message Top2  ", " Message Bottom ");
-    }
-    else if ( digitalRead( inputs.at(1) ) )
-    {
-        printMessage("  Message Top3  ", " Message Bottom ");
-    }
-    else if ( digitalRead( inputs.at(0) ) & digitalRead( inputs.at(1) ) )
+//    Serial.println(digitalRead(1), DEC);	
+
+    if (!digitalRead( 1 ) )
     {
         printMessage("  Message Top4  ", " Message Bottom ");
     }
